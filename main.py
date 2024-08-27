c3ed94a31cecf4140fc1e85a9abd999686d4f0b0
LETTING <la@Lettings-MacBook-Pro.local>
2024-08-27 16:42:17 +0300

    dash

--- main.py
+++ main.py
@@ -43,7 +43,10 @@ class Sale(db.Model):
 with app.app_context():
     db.create_all()
 
-CORS(app, resources={r"/product/*": {"origins": "http://127.0.0.1:5500"}})
+CORS(app, resources={r"/product/*": {"origins": "http://127.0.0.1:5500"}
+                     ,r"/sales/*": {"origins": "http://127.0.0.1:5500"},
+                     r"/dashboard/*":{"origins":"http://127.0.0.1:5500"}})
+
 
 
 @app.route('/product',methods=['GET','POST'])
@@ -55,6 +58,7 @@ def product():
             buying_price = data['buying_price']
             selling_price = data['selling_price']
             stock_quantity = data['stock_quantity']
+
             product = Product(name=name,buying_price=buying_price,selling_price=selling_price,stock_quantity=stock_quantity)
             db.session.add(product)
             db.session.commit()
@@ -62,16 +66,18 @@ def product():
         except Exception as e:
             return jsonify({'error':str(e)}),500
     elif request.method == 'GET':
-        products = db.session.execute(db.select(Product).order_by(Product.name)).scalars()
+        products = db.session.execute(db.select(Product).order_by(Product.id)).scalars()
+        prods =[]
         for product in products:
-            prods = [{
+            prods.append({
                 
+                "id":product.id,
                 "name":product.name,
                 "buying_price":product.buying_price,
                 "selling_price":product.selling_price,
                 "stock_quantity":product.stock_quantity
 
-            }]
+            })
         return jsonify({"products": prods}) ,200
 
 
@@ -90,13 +96,49 @@ def sales():
             return jsonify({"error":str(e)}),500
     elif request.method == 'GET':
         sales = db.session.execute(db.select(Sale).order_by(Sale.pid)).scalars()
+        sale_data = []
         for sale in sales:
-            sale_data =[{
-                'product': sale.product.name,
-                'quantity':sale.quantity
-            }]
+            sale_data.append({
+                'id':sale.id,
+                'product': sale.pid,
+                'quantity':sale.quantity,
+                'created_at':sale.created_at,
+            })
         return jsonify({"sales":sale_data}),200
     
+@app.route('/dashboard') 
+def dashboard():
+    
+    sales_per_day = db.session.query(
+        func.date(Sale.created_at).label('date'),
+        func.sum(Sale.quantity * Product.selling_price).label('total_sales')
+    ).join(Product).group_by(func.date(Sale.created_at)).all()
+
+    profit_per_day = db.session.query(
+        func.date(Sale.created_at).label('date'),
+        func.sum((Sale.quantity * Product.selling_price)-
+                (Sale.quantity * Product.buying_price)).label("profit")
+    ).join(Product).group_by(func.date(Sale.created_at)).all()
+
+    sales_data = [{'date':str(day),'total_sales':sales} for day,sales in sales_per_day]
+    profit_data =[{'date':str(day),'profit':profit} for day,profit in profit_per_day]
+
+    # sales_data=[]
+    # profit_data = []
+    # for day,sale in sales_per_day:
+    #     sales_data.append({
+    #         "day":day,
+    #         "total_sales":sale
+    #     })
+    # for day,profit in profit_per_day:
+    #     profit_data.append(
+    #         {
+    #             "day":day,
+    #             "profit":profit
+    #         }
+    #     )
+# ,{"profit_day":profit_data}
+    return jsonify({"sales_per_day":sales_data,"profit_per_day":profit_data}),200
 
 # testing sentry  
 @app.route('/sentry_error')
